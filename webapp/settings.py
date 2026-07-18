7209bad920133ea5d45938ce218ed0ca671649a5
Walimba Daka <23215729@airtel.africa>
2026-07-18 22:28:54 +0200

    master

--- webapp/settings.py
+++ webapp/settings.py
@@ -94,10 +94,21 @@ WSGI_APPLICATION = 'webapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}"""
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': '...',
+        'USER': '...',
+        'PASSWORD': '...',
+        'HOST': '...',
+        'PORT': '5432',
     }
 }
 
